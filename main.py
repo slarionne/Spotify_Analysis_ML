dccac92d04e4a2896fbb81a9c9c52acde8677ab7
Sebastien <slarionne@gmail.com>
2023-11-27 18:46:10 -0500

    ML add-on
    Additional ML analysis performed to determine feature importance.

--- main.py
+++ main.py
@@ -77,9 +77,6 @@ plt.pyplot.show()
 scaled_df2 = pd.DataFrame(scaled_df)
 
 scaled_df2.hist()
-plt.pyplot.show()
-
-
 
 
 # We are trying to predict the modality of a song, Minor or Major
@@ -105,4 +102,108 @@ plt.plot(depths, acc_depth)
 plt.xlabel('max_depth')
 plt.ylabel('accuracy')
 plt.show()
+
+# Find the largest accuracy and the depth at with it occurs
+max_acc = np.max(acc_depth)
+best_depth = depths[np.argmax(acc_depth)]
+print(f'Highest accuracy {round(max_acc,4)}% at depth {best_depth}')
+
+# Decision Tree Alg Fit
+dt_final = DecisionTreeClassifier(random_state = 42,
+                                  max_depth = best_depth,
+                                  max_leaf_nodes=17)
+dt_final.fit(train_data, train_labels)
+
+# Predict
+y_pred = dt_final.predict(test_data)
+
+metrics = classification_report(test_labels, y_pred)
+print("The metrics for a classification model")
+print(metrics)
+
+plt.figure(figsize=(16,10))
+tree.plot_tree(dt_final, feature_names = var,
+               class_names = ['Minor', 'Major'], # Major is represented by 1 and minor is 0.
+                filled=True)
+plt.show()
+
+# Predicting popularity
+
+#Random Forest Classification
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Splitting features and target
+x_data = df.drop('track_popularity',axis = 1)
+y_data = df['track_popularity']
+
+print(f'Shape of x_train {x_data.shape} and y_data {y_data.shape}')
+
+
+# Removing all identifying features
+
+"""All identifying features have the dtype of Object, and the only potentially useful features with that dtype are 
+Genre and Subgenre. However, for now, all "Objects" are excluded."""
+
+
+x_data = x_data.select_dtypes(exclude=['object'])
+x_data.head()
+
+sns.set(rc={'figure.figsize': (20, 16)})
+x_data.hist(color='skyblue')
+
+y_data.hist(color='orange',bins = 50)
+
+# Simplify Classification
+
+# Define class boundaries
+class_boundaries = [y_data.median(),100]
+
+# Create a new column in y_data representing the simplified classes
+y_data_class = np.digitize(y_data, bins=class_boundaries, right=True)
+
+# Convert class labels to numerical values
+num_classes = len(np.unique(y_data_class))
+
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data_class, test_size=0.2, random_state=42)
+
+
+# Building Model
+
+# Build a random forest classifier
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the classifier
+rf_classifier.fit(x_train, y_train)
+
+# Make predictions on the test set
+y_pred = rf_classifier.predict(x_test)
+
+# Evaluate the accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Random forest Accuracy:", accuracy)
+
+# Calculating feature importance
+
+feature_importances = rf_classifier.feature_importances_
+
+# Sorting the feature importances in descending order
+sorted_idx = feature_importances.argsort()
+
+# Plotting
+plt.figure(figsize=(10, 8))
+plt.barh(range(len(sorted_idx)), feature_importances[sorted_idx], align='center')
+plt.yticks(range(len(sorted_idx)), [x_data.columns[i] for i in sorted_idx])
+plt.title('Feature Importances in Random Forest Model')
+plt.xlabel('Relative Importance')
+plt.ylabel('Feature')
+plt.show()
+
+"""
+Results shows that the most import features include, loudness, diration_ms, tempo, and energy.
+
+"""
 
